30ed89833cef677d611fce24c2103abc7f403e4e
Ryan Urabe <rurabe@gmail.com>
2014-07-01 11:56:21 -1000

    fixed venue finder

--- lib/stubhub/venue.rb
+++ lib/stubhub/venue.rb
@@ -1,7 +1,7 @@
 module Stubhub
   class Venue < Document
-     def self.find_by_venue_id(venue_id, options = {})
-      params = { :venue_id => venue_id }
+    def self.find_by_id(venue_id, options = {})
+      params = { :id => venue_id }
       find(params, options).first
     end
 
