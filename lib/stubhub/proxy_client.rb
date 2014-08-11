51ff8a3d88d35040c850a775bf3d3fd0bcfc6193
Ryan Urabe <rurabe@gmail.com>
2014-08-11 13:12:54 -1000

    passed opts to proxy client

--- lib/stubhub/proxy_client.rb
+++ lib/stubhub/proxy_client.rb
@@ -1,7 +1,7 @@
 module Stubhub
   class ProxyClient < Client
     class << self
-      def get(url)
+      def get(url,opts={})
         uri = URI(url)
         Net::HTTP.new(uri.host,uri.port,ENV['PROXY_ADDRESS'],ENV['PROXY_PORT']).start do |http|
           http.get(uri)
