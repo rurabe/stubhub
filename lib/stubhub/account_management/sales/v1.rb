5fda9b7af5965ea1d192e47ca885c1a531da1a53
Ryan Urabe <rurabe@gmail.com>
2015-01-29 15:18:01 -0500

    addded event sales endpoint

--- lib/stubhub/account_management/sales/v1.rb
+++ lib/stubhub/account_management/sales/v1.rb
@@ -3,6 +3,10 @@ module Stubhub
     module Sales
       class V1
         class << self
+          def find_by_event_id(event_id,opts={})
+            Client.make_request("accountmanagement/sales/v1/event/#{event_id}",query(opts),opts.merge(context: :user))
+          end
+
           def get_sales(opts={})
             Client.make_request("accountmanagement/sales/v1/seller/#{Stubhub.user_id}",query(opts),opts.merge(context: :user))
           end
@@ -11,7 +15,6 @@ module Stubhub
 
             def query(opts)
               {
-                rows: 200,
                 includePending: true,
                 sort: "SALEDATE Desc"
               }.merge(opts[:query] || {})
