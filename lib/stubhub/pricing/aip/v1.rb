e493641e4e3339634a51a74ce8b3359020b9d500
Ryan Urabe <rurabe@gmail.com>
2015-01-29 20:41:46 -0500

    added refresh token and a less bespoke but more flexible AIP

--- lib/stubhub/pricing/aip/v1.rb
+++ lib/stubhub/pricing/aip/v1.rb
@@ -4,64 +4,33 @@ module Stubhub
       class V1 < Response
         class << self
 
-          # First parameter is an array of hashes, each including
-          #   :listing_id => the stubhub_id of the listing being priced,
-          #   :price      => the display_price
-          # Optionally, a type can be specified, which overrides the type hardcoded here
-          #   :type       => [:display,:listing,:payout]
-          #
-          def price_listing_as_display_price(listings,opts={})
+          # This method requires you to structure the individual listing requests the way they want it yourself
+          def price_listings(listings,opts={})
             response = Client.make_request(
               'pricing/aip/v1/price',
-              prepare_pricing_query(listings: listings, type: :display),
+              prepare_pricing_query(listings),
               opts.merge(method: :post, context: :user)
             )
-            self[response]
           end
 
           private
-             def prepare_pricing_query(params={})
+             def prepare_pricing_query(listings)
                 {
                   priceRequestList: {
-                    priceRequest: prepare_price_requests(params)
+                    priceRequest: listings
                   }
                 }
-              end
-
-              def prepare_price_requests(params)
-                params[:listings].map do |listing|
-                  prepare_price_request(listing,params)
-                end
-              end
-
-              def prepare_price_request(listing,params)
-                id = listing[:listing_id]
-                price = listing[:price]
-                {
-                  requestKey: id,
-                  listingId: id,
-                  amountPerTicket: {
-                    amount: price,
-                    currency: "USD"
-                  },
-                  amountType: price_type(listing,params),
-                  sellerId: 6686104,
-                }
-              end
-
-              def price_type(listing,params)
-                case (listing[:type] || params[:type])
-                when :display then "DISPLAY_PRICE"
-                when :listing then "LISTING_PRICE"
-                when :payout  then "PAYOUT"
-                end
               end
 
           end # /self
 
         def listing_price_for(request_key)
-          price_response = self['priceResponseList']['priceResponse'].find{|pr| pr['requestKey'] == request_key.to_s }
+          price_response = response_for(request_key)
           price_response['listingPrice']['amount'] if price_response
+        end
+
+        def response_for(request_key)
+          self['priceResponseList']['priceResponse'].find{|pr| pr['requestKey'] == request_key.to_s }
         end
       end
     end
--- lib/stubhub/user.rb
+++ lib/stubhub/user.rb
@@ -3,37 +3,51 @@ module Stubhub
     class << self
 
       def get_token(opts={})
-        opts[:key]      ||= Stubhub.consumer_key
-        opts[:secret]   ||= Stubhub.consumer_secret
-        opts[:username] ||= Stubhub.developer_username
-        opts[:password] ||= Stubhub.developer_password
-        response = Client.make_basic_request('login',{}, opts.merge(request: generate_request(opts)) )
-        JSON.parse(response.body).merge("user_id" => response['X-StubHub-User-GUID']) if response.body
+        request = generate_request(opts, grant_type: 'password')
+        response = parse_response(Client.make_basic_request('login',{}, opts.merge(request: request) ))
+        set_params!(response)
       end
 
-      def login!(opts={})
-        login_hash = get_token(opts)
-        Stubhub.configure(
-          consumer_token: login_hash["access_token"],
-          user_id: login_hash["user_id"]
-        )
-        true
+      def refresh_token(opts={})
+        opts[:refresh_token] ||= get_token["refresh_token"]
+        request = generate_request(opts, grant_type: 'refresh_token', refresh_token: opts[:refresh_token])
+        response = parse_response(Client.make_basic_request('login',{}, opts.merge(request: request) ))
+        set_params!(response)
       end
 
       private
 
-        def generate_request(opts)
+        def generate_request(opts={},form_data={})
+          opts[:key]      ||= Stubhub.consumer_key
+          opts[:secret]   ||= Stubhub.consumer_secret
+          opts[:username] ||= Stubhub.developer_username
+          opts[:password] ||= Stubhub.developer_password
           basic_authorization_token = Base64.strict_encode64("#{opts[:key]}:#{opts[:secret]}").strip
           Net::HTTP::Post.new(URI("https://api.stubhub.com/login")).tap do |r|
             r['Content-Type']  = "application/x-www-form-urlencoded"
             r['Authorization'] = "Basic #{basic_authorization_token}"
-            r.set_form_data(
-              grant_type: 'password',
-              username: opts[:username],
-              password: opts[:password],
-              scope: 'PRODUCTION'
-            )
+            r.set_form_data(default_form_data(opts).merge(form_data))
           end
+        end
+
+        def default_form_data(opts)
+          {
+            username: opts[:username],
+            password: opts[:password],
+            scope:    'PRODUCTION'
+          }
+        end
+
+        def parse_response(response)
+          JSON.parse(response.body).merge("user_id" => response['X-StubHub-User-GUID']) if response.body
+        end
+
+        def set_params!(params)
+          Stubhub.configure({}.tap do |c|
+            c[:consumer_token] = params['access_token'] if params['access_token']
+            c[:user_id]        = params['user_id'] if params['user_id']
+          end)
+          params
         end
     end
   end
